65e23721b53519568afc24f784e5947dc6ed58c1
StefanRCitiriga <73283856+StefanRCitiriga@users.noreply.github.com>
2024-01-13 00:12:58 +0200

    feature added: can place X and O

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,18 +1,35 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-const GameBoard = () => {
+const GameBoard = ({onSelectSquare, activePlayerSymbol}) => {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  const handleSelectSquare = (rowIndex, colIndex) => {
+    setGameBoard((prevBoard) => {
+      const updatedBoard = [...prevBoard.map((innerArray) => [...innerArray])];
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedBoard;
+    });
+
+    //part of lifting state up
+    onSelectSquare();
+  };
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
